95dada5de3b350428024c734c63834891cb542db
Shreya <shreyajoshi777@gmail.com>
2024-07-28 19:04:55 -0400

    added data

--- src/data.ts
+++ src/data.ts
@@ -2,33 +2,159 @@ export const timelineData = [
   {
     side: 'right',
     number: 1,
-    title: 'Lorem Ipsum',
-    date: 'week 1',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    image: 'img.JPG'
+    title: 'Orientation',
+    date: 'June 3rd - June 4th, 2024',
+    description: "Kickstarted my summer with orientation! It was great meeting all the interns, connecting with our program leads, and listening to AFS leadership share their experiences. I learned the skills necessary to dive into the exciting opportunities ahead!",
+    image: 'orient.JPG'
   },
   {
     side: 'left',
     number: 2,
-    title: 'Lorem Ipsum',
-    date: 'week 1',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    image: 'img.JPG'
+    title: 'Week 1: Finding my Project',
+    date: 'June 5th - June 7th, 2024',
+    description: "Finding my project was un unexpected adventure. When my original assignment became unavailable, I discovered how supportive the people and culture at AFS truly are. Rachel helped me navigate finding my current project and the internal interview process. This experience allowed me to realize the value of adaptability and having a supportive community.",
   },
   {
     side: 'right',
     number: 3,
-    title: 'Lorem Ipsum',
-    date: 'week 1',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    image: 'img.JPG'
+    title: 'ERG Showcase',
+    date: 'June 5th, 2024',
+    description: "Joining the ERG showcase presentation introduced me to the diverse communities at AFS. It's amazing to be in an environment that fosters inclusivity and supports employees in bring their authentic selves to work. I joined the Women's, DCAAT, Interfaith, and other ERGs.",
+    image: 'erg.JPG',
   },
   {
     side: 'left',
     number: 4,
-    title: 'Lorem Ipsum',
-    date: 'week 1',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    title: 'Week 2: Onboarding my Project',
+    date: 'June 10th - 14th, 2024',
+    description: "In my first week on my project, I did a deep dive into understanding the project's framework by exploring our blueprints and solution architecture diagrams. I also started technical tutorials to familiarize myself with our product platforms and technologies.",
+  },
+  {
+    side: 'right',
+    number: 5,
+    title: 'Meeting my MD Buddy: Kathryn Larson',
+    date: 'June 11th, 2024',
+    description: "",
+  },
+  {
+    side: 'left',
+    number: 6,
+    title: 'JHU Recruiting Bowling Celebration',
+    date: 'June 12th, 2024',
+    description: "",
+    image: 'jhu_bowling.JPG'
+  },
+  {
+    side: 'right',
+    number: 7,
+    title: 'Week 3 & 4: Project Work',
+    date: 'June 17th - 28th, 2024',
+    description: "",
+  },
+  {
+    side: 'left',
+    number: 8,
+    title: 'WERG Golf Scramble',
+    date: 'June 17th',
+    description: "",
+    image: 'golf.JPG'
+  },
+  {
+    side: 'right',
+    number: 9,
+    title: 'Meeting my MD sponsor: Maureen Brown',
+    date: 'June 21st, 2024',
+    description: "",
+  },
+  {
+    side: 'left',
+    number: 10,
+    title: 'Exploring GenAI Projects With Jay Chung',
+    date: 'June 25th, 2024',
+    description: "",
+  },
+  {
+    side: 'right',
+    number: 11,
+    title: 'Week 5: Project Work',
+    date: 'July 1st - 5th, 2024',
+    description: "",
+  },
+  {
+    side: 'left',
+    number: 12,
+    title: 'Student Leadership Conference at St. Charles, Illinois',
+    date: 'July 12th - 14th, 2024',
+    description: "",
+    image: 'slc.JPG'
+  },
+  {
+    side: 'right',
+    number: 13,
+    title: 'Week 7: Project Work',
+    date: 'July 15th - 19th, 2024',
+    description: "",
     image: 'img.JPG'
+  },
+  {
+    side: 'left',
+    number: 14,
+    title: 'Metro DC Office Welcome Event',
+    date: 'July 17th, 2024',
+    description: "",
+    image: 'metrodc.JPG'
+  },
+  {
+    side: 'right',
+    number: 15,
+    title: "Leading a WERG, Women's ERG Event",
+    date: 'July 18th, 2024',
+    description: "",
+    image: 'img.JPG'
+  },
+  {
+    side: 'left',
+    number: 16,
+    title: 'Reconnecting with my MD Sponsor, Maureen',
+    date: 'July 18th, 2024',
+    description: "",
+  },
+  {
+    side: 'right',
+    number: 17,
+    title: "Leadership Hot Ones Challenge",
+    date: 'July 18th, 2024',
+    description: "",
+  },
+  {
+    side: 'left',
+    number: 18,
+    title: 'Nationals Game',
+    date: 'July 23rd, 2024',
+    description: "",
+    image: 'nats.JPG'
+  },
+  {
+    side: 'right',
+    number: 19,
+    title: "Forge Workshop",
+    date: 'July 25th, 2024',
+    description: "",
+    image: 'xr.JPG'
+  },
+  {
+    side: 'left',
+    number: 20,
+    title: 'S&C Summer Social',
+    date: 'July 25th, 2024',
+    description: "",
+    image: 's&c_social.JPG'
+  },
+  {
+    side: 'right',
+    number: 21,
+    title: "AFS Health Portfolio Lunch & Learn",
+    date: 'July 26th, 2024',
+    description: "",
   },
 ];
