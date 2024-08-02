15f52eb2f9174e8ac5860c6506b482ddef90e2f0
Shreya <shreyajoshi777@gmail.com>
2024-08-02 00:27:35 -0400

    updated data

--- src/data.ts
+++ src/data.ts
@@ -12,7 +12,7 @@ export const timelineData = [
     number: 2,
     title: 'Week 1: Finding my Project',
     date: 'June 5th - June 7th, 2024',
-    description: "Finding my project was un unexpected adventure. When my original assignment became unavailable, I discovered how supportive the people and culture at AFS truly are. Rachel helped me navigate finding my current project and the internal interview process. This experience allowed me to realize the value of adaptability and having a supportive community.",
+    description: "Finding my project was an unexpected adventure. When my original assignment became unavailable, I discovered how supportive the people and culture at AFS truly are. Rachel helped me navigate finding my current project and the internal interview process. This experience allowed me to realize the value of adaptability and having a supportive community.",
   },
   {
     side: 'right',
@@ -34,14 +34,14 @@ export const timelineData = [
     number: 5,
     title: 'Meeting my MD Buddy: Kathryn Larson',
     date: 'June 11th, 2024',
-    description: "",
+    description: "Had an inspiring meeting with my MD buddy, Kathryn, who shared her journey at AFS and gave me valuable advice on making an impact during my internship: be flexible and actively seek opportunities to create value for your team. Kathryn also connected me to the Women's ERG, giving me the opportunity to plan my first WERG event!",
   },
   {
     side: 'left',
     number: 6,
     title: 'JHU Recruiting Bowling Celebration',
     date: 'June 12th, 2024',
-    description: "",
+    description: "Bowling with JHU alumni at AFS: It was a fun evening connecting and learning about everyone's experiences!",
     image: 'jhu_bowling.JPG'
   },
   {
@@ -49,50 +49,50 @@ export const timelineData = [
     number: 7,
     title: 'Week 3 & 4: Project Work',
     date: 'June 17th - 28th, 2024',
-    description: "",
+    description: "During these weeks, I researched authentication methods for the product our team is building, explored building APIs in the AWS API Gateway, utilized Postman to test and debug authentication requests, and understood a basic lambda function used to handle authentication processes.",
   },
   {
     side: 'left',
     number: 8,
     title: 'WERG Golf Scramble',
     date: 'June 17th',
-    description: "",
+    description: "My first WERG event! Enjoyed a golf workshop and scramble with the WERG- a great opportunity to spend time with other interns and meet analysts in the WERG.",
     image: 'golf.JPG'
   },
   {
     side: 'right',
     number: 9,
-    title: 'Meeting my AFS Supervisor: Richard Lord',
+    title: 'Meeting with my project Senior Manager: Richard Lord',
     date: 'June 20th, 2024',
-    description: "",
+    description: "I had a valuable discussion with my AFS supervisor, learning about his inspiring journey and experiences. He also spoke to the entrepreneurial nature of work at AFS, something that also resonates with me.",
   },
   {
     side: 'left',
     number: 9,
     title: 'Meeting my MD sponsor: Maureen Brown',
     date: 'June 21st, 2024',
-    description: "",
+    description: "An insightful conversation with my Managing Director sponsor, Maureen, who shared her path at AFS and gave me advice on exploring and navigating my career. Maureen also introduced me to a GenAI project on our account, an area I'm interesting in exploring!",
   },
   {
     side: 'right',
     number: 10,
     title: 'Exploring GenAI Projects With Jay Chung',
     date: 'June 25th, 2024',
-    description: "",
+    description: "Connected with Jay in an informative discussion on the generative AI projects on my account, including aspects of responsible AI, security, transparency with clients, and risks. It was fascinating to explore our GenAI initiatives and understand AFS' impact in this growing area.",
   },
   {
     side: 'left',
     number: 11,
     title: 'Week 5: Project Work',
     date: 'July 1st - 5th, 2024',
-    description: "",
+    description: "This week, I focused on mapping and identifying discrepancies between expected API specifications and provided OpenAPI specifications, classified and added blockers for JIRA stories, and learned the fundamentals of JSON.",
   },
   {
     side: 'right',
     number: 12,
     title: 'Student Leadership Conference at St. Charles, Illinois',
     date: 'July 12th - 14th, 2024',
-    description: "",
+    description: "It was an incredible experience attending the Accenture Student Leadership Conference at Q Center in St. Charles, Illinois. We heard from inspiring speakers like Nellie Borrero and Beck Bailey on diversity and inclusion, strengths development, and leadership skills. It was also a great opportunity to connect with interns from across North America and learn about their experiences in Accenture LLP.",
     image: 'slc.JPG'
   },
   {
@@ -100,45 +100,44 @@ export const timelineData = [
     number: 13,
     title: 'Week 7: Project Work',
     date: 'July 15th - 19th, 2024',
-    description: "",
-    image: 'img.JPG'
+    description: "I worked towards performing integration testing of provided APIs and learning schemathesis, a automated testing tool, utilized to improve the efficiency of our testing process.",
   },
   {
     side: 'right',
     number: 14,
     title: 'Metro DC Office Welcome Event',
     date: 'July 17th, 2024',
-    description: "",
+    description: "I had an exciting opportunity to visit the metro DC office, hearing from the office MD, Ed Engles, share his journey, complete with the ups and downs. I loved seeing the collaboratie office spaces and connecting with the LLP interns.",
     image: 'metrodc.JPG'
   },
   {
     side: 'left',
     number: 15,
-    title: "Leading a WERG, Women's ERG Event",
+    title: "Leading a WERG (Women's ERG) Event",
     date: 'July 18th, 2024',
-    description: "",
-    image: 'img.JPG'
+    description: "Hosted the watch party for the WERG panel: Empowering Women Analysts- Strategies for Success and Impact. It was an amazing experience collaborating with the WERG's communications lead, Avery McNamee, to organize a successful in-person event. It was a fantastic opportunity to connect and engage with the WERG!",
+    image: 'werg_party.JPG'
   },
   {
     side: 'right',
     number: 16,
     title: 'Reconnecting with my MD Sponsor, Maureen',
     date: 'July 18th, 2024',
-    description: "",
+    description: "I had a valuable coffee-chat with my project MD sponsor, gaining a deeper understanding of our client-facing work and our current challenges in the project as well as the importance of building strong client relationships. Something that resonated with me from her personal experiences is: avoiding perfectionism- providing practical and quality work.",
   },
   {
     side: 'left',
     number: 17,
     title: "Leadership Hot Ones Challenge",
     date: 'July 18th, 2024',
-    description: "",
+    description: "Watched our company's leadership take on the Hot Ones challenge, sharing their experiences at AFS and advice for the interns. Grateful to Rachel and Ali & Ali for planning this for us!",
   },
   {
     side: 'right',
     number: 18,
     title: 'Nationals Game',
     date: 'July 23rd, 2024',
-    description: "",
+    description: "Super fun getting to know other Analyst buddies and watching the Nats vs. Padres game with all the interns!",
     image: 'nats.JPG'
   },
   {
@@ -146,7 +145,7 @@ export const timelineData = [
     number: 19,
     title: "Forge Workshop",
     date: 'July 25th, 2024',
-    description: "",
+    description: "Visiting the Forge was an exciting day! I experienced demos of AFS' FedGPT and XR technology/initatives, learned about creating effective presentations, and collaborated with other interns on a case study. To improve economic stability, we proposed a deep learning model which predicts economic trends to advise the Federal Reserve's policies and decisions.",
     image: 'xr.JPG'
   },
   {
@@ -154,41 +153,34 @@ export const timelineData = [
     number: 20,
     title: 'S&C Summer Social',
     date: 'July 25th, 2024',
-    description: "",
+    description: "Had a great time at the S&C summer social, connecting with the S&C team at AFS and understanding their work!",
     image: 's&c_social.JPG'
   },
   {
     side: 'left',
     number: 21,
-    title: "AFS Health Portfolio Lunch & Learn",
-    date: 'July 26th, 2024',
-    description: "",
+    title: "Ron Ash's Journey Line",
+    date: 'July 29th, 2024',
+    description: "We had the privilege of hearing from our new CEO, Ron Ash, who shared invaluable advice and insights. He took us through the pivotal moments and challenges that shaped his career, and gave us great advice as we start our careers. After hearing his initiatives for AFS, I am incredibly inspired and motivate for the future!",
+    image: ''
   },
   {
     side: 'right',
     number: 22,
-    title: "Ron Ash's Journey Line",
-    date: 'July 29th, 2024',
-    description: "",
-    image: ''
+    title: "TSA Innovation Day",
+    date: 'July 30th, 2024',
+    description: "It was a really exciting experience attending the TSA Innovation Day for interns, featuring demos on the application of XR and AI technologies in the government space. I loved asking questions about the real-time voice detection and comprehension incorporated in the presented technology and the possibility of sentiment analysis or 3D object segmentation in future TSA technology. It was fascinating to hear from experts in the field and witness advanced technologies tested for deployment!",
   },
   {
     side: 'left',
     number: 23,
-    title: "TSA Innovation Day",
-    date: 'July 30th, 2024',
-    description: "",
-  },
-  {
-    side: 'right',
-    number: 24,
     title: 'Meeting with Danny Cooper: Accenture Leadership',
     date: 'July 31st, 2024',
     description: ""
   },
   {
-    side: 'left',
-    number: 25,
+    side: 'right',
+    number: 24,
     title: "Meeting with Nathan Fleming: Business Process Arch Senior Manager",
     date: 'August 1st, 2024',
     description: "",
