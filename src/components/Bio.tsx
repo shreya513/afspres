e3a83cab962360181adad72b27e8dead9330c685
Shreya <shreyajoshi777@gmail.com>
2024-08-06 00:45:13 -0400

    edits

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -10,7 +10,7 @@ const Bio: React.FC = () => {
           At school, I'm a teaching assistant for Data Structures and am involved in Machine Learning research at the Johns Hopkins Hospital Cognitive Neurophysiology and 
           Brain-Machine Interfaces Lab, working on a project on detecting speech from neural signals. I'm serving as the President of the Johns Hopkings campus event programming 
           board, am involved in the JHU Women in CS club, and perform violin for patients in local hospitals. This past semester I was also a Spanish translator for TEDx and a 
-          volunteer at a local Baltimore Public School, teaching and translating for Spanish-speaking students and parents. Outsiside of school, I love hiking, baking, and 
+          volunteer at a local Baltimore Public School, teaching and translating for Spanish-speaking students and parents. Outside of school, I love hiking, baking, and 
           traveling!<br className="break"></br>
           This summer, I've had an incredible journey full of growth, adventure, and amazing people. This timeline showcases the highlights of my experiences! :)
         </p>
